d3a943110616fad1f9fa3f90d7a53bb4f9ccbfe8
Stefano <stefano.pola.fargas@gmail.com>
2021-01-06 13:34:54 +0100

    Creacio dels elements i dels metodes getters i setters

--- src/main/java/com/domain/Employee.java
+++ src/main/java/com/domain/Employee.java
@@ -3,10 +3,78 @@
  */
 package com.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author stefano
  *
  */
 public class Employee {
+	
+	private static int id=1;
+	private double salary;
+	private String name;
+	
+	@Autowired
+	private Jobs job;
+	/**
+	 * @return the id
+	 */
+	
+	public Employee () {
+		
+		id=id++;
+		
+	}
+	
+	public static int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public static void setId(int id) {
+		Employee.id = id;
+	}
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the job
+	 */
+	public Jobs getJob() {
+		return job;
+	}
+	/**
+	 * @param job the job to set
+	 */
+	public void setJob(Jobs job) {
+		this.job = job;
+	}
+	
+	
+	
+	
 
 }
