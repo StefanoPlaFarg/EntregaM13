5703d1fa300a9ff49fdcd2e8517e619edfb6c133
Stefano <stefano.pola.fargas@gmail.com>
2021-01-06 14:15:46 +0100

    Canvi de nom de la classe i creació de tots els metodes

---
+++ src/main/java/com/service/EmployeeService.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.domain.Employee;
+
+/**
+ * @author stefano
+ *
+ */
+
+@Service
+public class EmployeeService {
+
+	List<Employee> employeeRepository = new ArrayList<Employee>();
+	
+	
+	//Create
+	
+	public String saveEmployee(Employee employee) {
+
+		employeeRepository.add(employee);		
+		return "Employee saved";
+
+	}
+	
+	//Read
+	public Employee getEmployeeById(int id) {
+
+		if (employeeRepository.get(id) != null) {
+
+			return employeeRepository.get(id);
+
+		}
+
+		return null;
+
+	}
+	
+	
+	//Update
+	public String updateEmployee (int id, Employee employeeUpdated) {
+		
+		if (employeeRepository.get(id) != null) {
+
+			employeeRepository.get(id).setJob(employeeUpdated.getJob());
+			employeeRepository.get(id).setName(employeeUpdated.getName());
+			employeeRepository.get(id).setSalary(employeeUpdated.getSalary());
+			 
+			
+			return "Employee updated";
+
+		}
+		
+		return "Employee not found";
+		
+	}
+	
+
+    //Delete
+     public String deleteEmployee (int id) {
+		
+		if (employeeRepository.get(id) != null) {
+
+			employeeRepository.remove(id);
+			
+			return "Employee deleted";
+
+		}
+		
+		return "Employee not found";
+	}
+	
+	
+	
+}
